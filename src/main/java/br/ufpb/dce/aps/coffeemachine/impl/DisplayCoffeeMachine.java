4dc20d03a2fd2afeef9d254615256dccd38607b9
Fernando M. Oliveira <fernando.dsfw@gmail.com>
2014-07-15 09:40:59 -0300

    Task 1.03 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/DisplayCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/DisplayCoffeeMachine.java
@@ -6,6 +6,9 @@ import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
 public class DisplayCoffeeMachine extends Component {
+	
+	private int dollar = 0;
+	private int cents = 0;
 
 	public DisplayCoffeeMachine(String name) {
 		super(name);
@@ -13,7 +16,18 @@ public class DisplayCoffeeMachine extends Component {
 
 	@Service(name = "mostrarValorInserido")
 	public void mostrarValorInserido(Coin dime, ComponentsFactory factory) {
-		Double valor = (double) (dime.getValue() % 100);
-		factory.getDisplay().info("Total: US$ 0." + dime.getValue());
+		this.dollar += dime.getValue() / 100;
+		this.cents += dime.getValue() % 100;
+		
+		factory.getDisplay().info("Total: US$ " + dollar + "." + cents);
 	}
+
+//	public double getTotal() {
+//		double centsWithZero = cents % 10 > 1  ? new Double(cents + "0") : cents; 
+//		return dollar + centsWithZero; 
+//	}
+
+	
+	
+	
 }
